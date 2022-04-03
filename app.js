9cacf9a001390d2836bed15aa495368257c02f15
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-04-03 20:40:41 +0600

    slide-show-game

---
+++ app.js
@@ -0,0 +1,16 @@
+const slideShowElements = document.querySelectorAll('.slide-element');
+
+let countElement = 1;
+setInterval(() => {
+    countElement++;
+    let currentElement = document.querySelector(".current");
+    currentElement.classList.remove("current");
+
+    if (countElement > slideShowElements.length) {
+        slideShowElements[0].classList.add('current')
+        countElement = 1
+    } else {
+        currentElement.nextElementSibling.classList.add("current")
+    }
+
+}, 2000);
